6723589179553dc279e2105beee5505c183c0c3a
JanSharp <steinmeister36@gmail.com>
2021-05-06 16:34:51 +0200

    Remove trailing newlines from lambdas

--- preprocessor.lua
+++ preprocessor.lua
@@ -70,16 +70,16 @@ end
 ---@return string
 local function preprocess_lambda_expressions(chunk)
   chunk = chunk:gsub("([a-zA-Z_][a-zA-Z0-9_]*)%s*=>%s*(%b())", function(param, body)
-    return " function("..param..") return "..body:sub(2, -2)..";end\n"
+    return " function("..param..") return "..body:sub(2, -2)..";end "
   end)
   chunk = chunk:gsub("([a-zA-Z_][a-zA-Z0-9_]*)%s*=>%s*(%b{})", function(param, body)
-    return " function("..param..")"..body:sub(2, -2)..";end\n"
+    return " function("..param..")"..body:sub(2, -2)..";end "
   end)
   chunk = chunk:gsub("(%([^())]*%))%s*=>%s*(%b())", function(params, body)
-    return " function"..params.." return "..body:sub(2, -2)..";end\n"
+    return " function"..params.." return "..body:sub(2, -2)..";end "
   end)
   chunk = chunk:gsub("(%([^())]*%))%s*=>%s*(%b{})", function(params, body)
-    return " function"..params..body:sub(2, -2)..";end\n"
+    return " function"..params..body:sub(2, -2)..";end "
   end)
   return chunk
 end
